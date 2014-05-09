94a5daf53d883a47392039ba60c4a94577c96e7a
RDS-Team <rds@redi-bw.de>
2014-05-09 11:51:19 +0200

    base theme for rds extends bootstrap

---
+++ themes/rds/theme.config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'extends' => 'bootstrap',
+    'css' => array(
+	'overwrite.css',
+	'rds-cd.css',
+    ),
+    'js' => array(
+    ),
+    'favicon' => 'rds-favicon.ico',
+);
