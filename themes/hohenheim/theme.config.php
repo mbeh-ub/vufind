d84939d1124d3d9fda4468e3a21390e9941db5c5
Jochen Lienhard <lienhard@ub.uni-freiburg.de>
2015-04-17 12:33:59 +0200

    RDS: work in progress - theme for hohenheim

---
+++ themes/hohenheim/theme.config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'extends' => 'rds',
+    'css' => array(
+	'overwrite.css',
+	'hohenheim-cd.css',
+        'hohenheim-location.css',
+    ),
+    'js' => array(
+    ),
+    'favicon' => 'hohenheim-favicon.ico',
+);
