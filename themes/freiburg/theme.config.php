b6aed4b05455642b752146d9ba25c0aef2ddc367
root <root@www-dev.ub.uni-freiburg.de>
2015-07-16 12:10:01 +0200

    Initial Commit of new template for freiburg

--- themes/freiburg/theme.config.php
+++ themes/freiburg/theme.config.php
@@ -2,9 +2,17 @@
 return array(
     'extends' => 'rds',
     'css' => array(
-	    'freiburg.css',
+		//'/css/bootstrap.min.css', // FIXME: We don't need to include it. Inherited from bootstrap theme
+		'/css/screen.css'
     ),
     'js' => array(
+		'/js/vendor/modernizr.custom.2.8.3.min.js',
+		'/js/vendor/jquery-ui-11.1.4.custom.min.js',
+		'/js/vendor/jquery.dataTables.min.js',
+		'/js/app.js',
+		'/js/plugin.mainnavigation.js',
+		'/js/plugin.metanavigation.js',
+		'/js/plugin.autoheighter.js'
     ),
     'favicon' => 'freiburg-favicon.ico',
 );
