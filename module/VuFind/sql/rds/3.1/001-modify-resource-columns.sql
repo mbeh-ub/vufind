e3954d40a2b0d8ac6e5fe9455d488c296ba11f8f
Markus Beh <markus.beh@ub.uni-freiburg.de>
2017-01-19 10:13:46 +0100

    add database migration scripts

---
+++ module/VuFind/sql/rds/3.1/001-modify-resource-columns.sql
@@ -0,0 +1,8 @@
+-- 
+-- Modifications to table `resource`
+--
+
+ALTER TABLE "resource" 
+	ALTER COLUMN record_id TYPE varchar;
+ 	ALTER COLUMN title TYPE varchar;
+	ALTER COLUMN author TYPE varchar;
---
+++ module/VuFind/sql/rds/3.1/002-add-record-columns.sql
@@ -0,0 +1,7 @@
+-- 
+-- Modifications to table `record`
+--
+
+ALTER TABLE "record" 
+	ADD COLUMN user_id int;
+	ADD COLUMN resource_id int; 
